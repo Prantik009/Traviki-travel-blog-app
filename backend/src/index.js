58b11390ac9a456d82b2660e473fd257be318c7d
WebHault108 <biswasritam5.13@gmail.com>
2025-08-07 23:49:38 +0530

    update frontend uri in backend cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,10 +12,20 @@ import { app, server } from "./lib/socket.js"
 
 dotenv.config()
 
+// app.use(cors({
+//   origin: process.env.NODE_ENV === "production" ? ["https://traviki-travel-blog-app.onrender.com"] : ["http://localhost:5173"],
+//   credentials: true,
+// }));
+
+
 app.use(cors({
   origin: process.env.NODE_ENV === "production" ? ["https://traviki-travel-blog-app.onrender.com"] : ["http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 }));
+
+app.options("*", cors()); // Optional fallback
 
 app.use(express.json())
 app.use(cookieParser())
