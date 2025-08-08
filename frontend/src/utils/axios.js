1bae12534d9c536c32ced43dd8ed33fdceeac47d
WebHault108 <biswasritam5.13@gmail.com>
2025-08-08 10:09:40 +0530

    frontend update:backend uri

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 // utils/axios.js
 import axios from "axios";
 
-const BASE_URL = "https://traviki-travel-blog-app-backend.onrender.com";
+const BASE_URL = "https://traviki-travel-blog-app-backend.onrender.com/api";
 
 export default axios.create({
   baseURL: BASE_URL,
