9f36c213f8a33eb35d266e0c5bb72ee6191fa669
WebHault108 <biswasritam5.13@gmail.com>
2025-08-08 12:05:23 +0530

    backend update: missing token fixed

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res)=> {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none',
-        secure: false
+        secure: true
     })
     // return cookie 
     return token
