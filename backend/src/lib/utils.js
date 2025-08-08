c81b88acc090f6462cf4128f9c6a80ac5ae753f5
WebHault108 <biswasritam5.13@gmail.com>
2025-08-08 18:54:53 +0530

    files updated

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res)=> {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none',
-        secure: true
+        secure: false
     })
     // return cookie 
     return token
