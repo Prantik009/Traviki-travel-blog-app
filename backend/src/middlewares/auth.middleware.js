093189d5ff6b947af901bf61f930292a605b5fe8
WebHault108 <biswasritam5.13@gmail.com>
2025-08-08 09:19:56 +0530

    backend update:middleware error handler added

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,20 +1,26 @@
-import jwt from "jsonwebtoken"
-import User from "../models/user.model.js"
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
-export const protectedRoute = async (req, res, next)=> {
-    try {
-        const token = req.cookies.jwt;
-        if (!token) return res.status(401).json({ message: "Unautorised - No token Provided"})
-        const decoded = jwt.verify(token, process.env.JWT_TOKEN)
-        if(!decoded) return res.status(401).json({ message: "Unauthorised - Invalid Token"}) 
-        
-        const user = await User.findById(decoded.userId).select("-password")
-        if (!user) return res.status(404).json({ message: " User not found."})
+export const protectedRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token)
+      return res
+        .status(401)
+        .json({ message: "Unautorised - No token Provided" });
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+    if (!decoded)
+      return res.status(401).json({ message: "Unauthorised - Invalid Token" });
 
-        req.user = user; // intialize the user found by id to the body  and send it to next function
-        
-        next()
-    } catch (error) {
-        
-    }
-}
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) return res.status(404).json({ message: " User not found." });
+
+    req.user = user; // intialize the user found by id to the body  and send it to next function
+
+    next();
+  } catch (error) {
+    console.log("error in middleware", error.message);
+    
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
