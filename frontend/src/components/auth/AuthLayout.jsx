1073e1704aef02134e6048b841b9248547652765
WebHault108 <biswasritam5.13@gmail.com>
2025-08-09 07:08:49 +0530

    logout fix:authlayout

--- frontend/src/components/auth/AuthLayout.jsx
+++ frontend/src/components/auth/AuthLayout.jsx
@@ -1,15 +1,16 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 export const AuthLayout = ({ children, authentication = true }) => {
   const { isAuthenticated, loading } = useSelector((state) => state.auth);
+  const location = useLocation()
 
   if (loading) {
     return <div className="loading">Loading...</div>;
   }
 
   if (authentication && !isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/login" state={{from: location}} replace />;
   }
   if (!authentication && isAuthenticated) {
     return <Navigate to="/" replace />;
