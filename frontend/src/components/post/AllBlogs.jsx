8988c1e2b2a88439ccdd825a61776c00d4fc9d06
WebHault108 <biswasritam5.13@gmail.com>
2025-08-09 07:32:57 +0530

    logout fix:authlayout

--- frontend/src/components/post/AllBlogs.jsx
+++ frontend/src/components/post/AllBlogs.jsx
@@ -13,37 +13,46 @@ export const AllBlogs = () => {
   const { allposts, loading } = useSelector((state) => state.blog);
   // console.log("All posts: ", allposts);
 
-  if (loading) return <>loading...</>;
+  if (loading)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
 
-  if (allposts.length<1) return (
-    <div className="pt-20 bg-gray-100 w-full flex flex-col items-center justify-center h-[100vh] ">
-       <p className=" place-items-center text-4xl font-nerko">No Post Yet</p>
-       <p>Please <span className="text-blue-600 underline"><Link to="/add-posts">add a post</Link></span></p>
-    </div>
-  )
+  if (allposts.length < 1)
+    return (
+      <div className="pt-20 bg-gray-100 w-full flex flex-col items-center justify-center h-[100vh] ">
+        <p className=" place-items-center text-4xl font-nerko">No Post Yet</p>
+        <p>
+          Please{" "}
+          <span className="text-blue-600 underline">
+            <Link to="/add-posts">add a post</Link>
+          </span>
+        </p>
+      </div>
+    );
 
   return (
     <div className="blogs py-4 place-items-center">
-        <div className=" w-full max-w-4xl flex-col mt-10 px-2">
-          <div className="blogs-cards w-full space-y-4">
-            {allposts.map((post) => (
-              <div
-                key={post._id}
-              >
-                <BlogCard
-                  title={post.blogTitle}
-                  content={post.blogContent?.slice(0, 150)}
-                  authorName={post.authorId.fullName}
-                  authorProfile={post.authorId.profilePic}
-                  postDate={post.createdAt?.slice(0, 10)}
-                  blogLocation={post.blogLocation}
-                  blogImg={post.blogImage}
-                  slug={post.slug}
-                />
-              </div>
-            ))}
-          </div>
+      <div className=" w-full max-w-4xl flex-col mt-10 px-2">
+        <div className="blogs-cards w-full space-y-4">
+          {allposts.map((post) => (
+            <div key={post._id}>
+              <BlogCard
+                title={post.blogTitle}
+                content={post.blogContent?.slice(0, 150)}
+                authorName={post.authorId.fullName}
+                authorProfile={post.authorId.profilePic}
+                postDate={post.createdAt?.slice(0, 10)}
+                blogLocation={post.blogLocation}
+                blogImg={post.blogImage}
+                slug={post.slug}
+              />
+            </div>
+          ))}
         </div>
       </div>
+    </div>
   );
 };
